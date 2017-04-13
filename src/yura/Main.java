08471eb7029be787870c723908e26bdb359e040d
Yura <yterletskyi@gmail.com>
2017-04-13 16:37:42 +0300

    Try randomize

--- src/yura/Main.java
+++ src/yura/Main.java
@@ -1,11 +1,9 @@
 package yura;
 
-import yura.rtp.Range;
 import yura.rtp.WinRandom;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
 public class Main {
 
@@ -23,52 +21,56 @@ public class Main {
         int win = 0;
 
         double[] winnings = {800, 160, 80, 50, 40, 25, 20, 10, 5, 2, 0};
-        double[] probabilities = calcProbabilities(winPercent / 100, 0.76171);
+        double[] probabilities = metaCalcProbabilities(winPercent / 100, rtp, winnings);
 
-
-        int multiplier = multiplier(probabilities, 100);
-
-        System.out.println("checking rtp: " + sumProduct(probabilities, winnings));
-
-        int size = probabilities.length;
-        for (int i = 0; i < size; i++) {
-            double prob = probabilities[i];
-            prob *= multiplier;
-            prob = Math.round(prob);
-            probabilities[i] = prob;
+        for (double p : probabilities) {
+            System.out.println(p);
         }
 
-        System.out.println("multiplier: " + multiplier);
-
-        Map<Range<Double>, Integer> winRangeMap = new HashMap<>();
-        double prevValue = 0;
-        for (int i = 0; i < winnings.length; i++) {
-            double iWin = winnings[i];
-            double iProb = probabilities[i];
-            winRangeMap.put(new Range<>(prevValue, iProb + iProb), (int) iWin);
-            prevValue += iProb;
-        }
-
-        double total = sum(probabilities);
-
-        for (int i = 0; i < 1_000_000; i++) {
-
-            double rand = winRandom.randIntIncluding((int) total);
-
-            for (Map.Entry<Range<Double>, Integer> entry : winRangeMap.entrySet()) {
-                Range<Double> range = entry.getKey();
-                Integer winInteger = entry.getValue();
-
-                if (range.isInRange(rand)) {
-                    win = winInteger;
-                }
-            }
-            totalWin += win;
-            totalBet += bet;
-        }
-
-        double rtpCheck = (double) totalWin / (double) totalBet;
-        System.out.println("rtp is: " + rtpCheck);
+//
+//        int multiplier = multiplier(probabilities, 100);
+//
+//        System.out.println("checking rtp: " + sumProduct(probabilities, winnings));
+//
+//        int size = probabilities.length;
+//        for (int i = 0; i < size; i++) {
+//            double prob = probabilities[i];
+//            prob *= multiplier;
+//            prob = Math.round(prob);
+//            probabilities[i] = prob;
+//        }
+//
+//        System.out.println("multiplier: " + multiplier);
+//
+//        Map<Range<Double>, Integer> winRangeMap = new HashMap<>();
+//        double prevValue = 0;
+//        for (int i = 0; i < winnings.length; i++) {
+//            double iWin = winnings[i];
+//            double iProb = probabilities[i];
+//            winRangeMap.put(new Range<>(prevValue, iProb + iProb), (int) iWin);
+//            prevValue += iProb;
+//        }
+//
+//        double total = sum(probabilities);
+//
+//        for (int i = 0; i < 1_000_000; i++) {
+//
+//            double rand = winRandom.randIntIncluding((int) total);
+//
+//            for (Map.Entry<Range<Double>, Integer> entry : winRangeMap.entrySet()) {
+//                Range<Double> range = entry.getKey();
+//                Integer winInteger = entry.getValue();
+//
+//                if (range.isInRange(rand)) {
+//                    win = winInteger;
+//                }
+//            }
+//            totalWin += win;
+//            totalBet += bet;
+//        }
+//
+//        double rtpCheck = (double) totalWin / (double) totalBet;
+//        System.out.println("rtp is: " + rtpCheck);
 
 
     }
@@ -106,15 +108,36 @@ public class Main {
         return sum;
     }
 
-    private static double[] calcProbabilities(double max, final double k) {
+    private static double[] metaCalcProbabilities(double max, final double rtp, double[] wins) {
+        double[] values = calcProbabilities(max);
+
+        boolean isRtpCorrect = isRtpCorrect(wins, values, rtp);
+        while (!isRtpCorrect) {
+            values = calcProbabilities(max);
+            isRtpCorrect = isRtpCorrect(wins, values, rtp);
+        }
+        return values;
+    }
+
+    private static boolean isRtpCorrect(double[] wins, double[] values, double rtp) {
+        return sumProduct(wins, values) - rtp <= 0.1;
+    }
+
+    private static double[] calcProbabilities(double max) {
         int size = 11;
         double[] probabilities = new double[size];
         probabilities[size - 1] = 1 - max;
         for (int i = 0; i < size - 1; i++) {
-            double dif = max * k;
-            max = max - dif;
-            probabilities[i] = dif;
+            double prob = randomDoubleInRange(max);
+            probabilities[i] = prob;
+            max -= prob;
         }
         return probabilities;
+    }
+
+    private static double randomDoubleInRange(double upperBound) {
+        Random r = new Random();
+        double rangeMin = 0.000001;
+        return rangeMin + (upperBound - rangeMin) * r.nextDouble();
     }
 }
